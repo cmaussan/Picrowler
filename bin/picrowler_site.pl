92c8636196dea30b49c471f5e14071e419f5a99d
cmaussan <cmaussan@linkfluence.net>
2010-04-09 18:40:44 +0200

    working version, added db and export

--- bin/picrowler_site.pl
+++ bin/picrowler_site.pl
@@ -8,8 +8,9 @@ use LWP::UserAgent;
 use URI;
 use Web::Scraper;
 
-### Gestion des params de ligne de commande ###
+$|=1;
 
+### Gestion des params de ligne de commande ###
 my ( $max_depth, $max_pages_count, $max_distance, @seed_urls ) = @ARGV or die( 'need a lot of things ;)' );
 
 print "Picrowler ---\n";
@@ -18,6 +19,21 @@ print "max_pages_count = $max_pages_count\n";
 print "max_distance = $max_distance\n";
 
 ### Gestion de la base de données ###
+my $nodes;
+my $edges;
+my $nodes_count = 0;
+
+sub find_or_create_node {
+    my $url = shift;
+    my $id  = undef;    
+    if( defined $nodes->{ $url } ) {
+        $id = $nodes->{ $url };
+    }
+    else {
+        $id = $nodes->{ $url } = ++$nodes_count; 
+    }
+    return $id;
+}
 
 ### Initialisation de LWP ###
 my $agent = LWP::UserAgent->new;
@@ -28,27 +44,49 @@ my $scraper = scraper {
     process "a", "links[]" => '@href';
 };
 
-my @sites_to_visit = grep { is_valid( $_ ) } @seed_urls;
+my @sites_to_visit = map { $_->canonical->as_string } grep { defined $_ } map { site_uri( $_ ) } @seed_urls;
 my @sites_already_visited = ();
 
 my $distance = 0;
 
 while( $distance <= $max_distance ) {
+
+    my @sites_outlinks = ();
     
-    for my $site_start_url ( @sites_to_visit ) {
+    for my $site_to_visit ( @sites_to_visit ) {
 
-        next unless( is_valid( $site_start_url ) );
+        next if( grep{ $_ eq $site_to_visit } @sites_already_visited );
 
-        my $site_site_uri = site_uri( $site_start_url );
-        next if( grep{ $_ eq $site_site_uri->canonical->as_string } @sites_already_visited );
+        print "crawling [$site_to_visit] ($distance) :\n";
+        my $id_source = find_or_create_node( $site_to_visit );
 
-        push @sites_to_visit, @{ crawler( $site_start_url, $site_site_uri ) };
-        push @sites_already_visited, $site_site_uri->canonical->as_string;     
+        push @sites_outlinks, { id_source => $id_source, links => crawler( $site_to_visit ) };
+        push @sites_already_visited, $site_to_visit;
 
-        $distance++;
     }
 
+    for my $site_relations ( @sites_outlinks ) {
+        my $id_source = $site_relations->{ id_source };
+
+        for my $site_outlink ( @{ $site_relations->{ links } } ) {
+            my $site_found = site_uri( $site_outlink );
+            next unless( $site_found );
+
+            my $id_target = find_or_create_node( $site_found );
+            $edges->{ "$id_source-$id_target" } ++ unless( $id_source == $id_target );
+
+            next if( grep{ $_ eq $site_found->canonical->as_string } @sites_already_visited );
+            push @sites_to_visit, $site_found; 
+        }
+    }
+
+    $distance++;
+
 }
+
+export();
+
+print "end crawling.\n";
 
 ### Vérificateur d'URL ###
 sub is_valid {
@@ -77,54 +115,41 @@ sub site_uri {
 
 sub is_outlink {
     my ( $site_uri, $link_uri ) = @_;
-    my $site_pattern = $site_uri->canonical->as_string;
-    return ( $link_uri->canonical->as_string !~ /^$site_pattern/ ) ? 1 : 0;
+    return ( $link_uri->canonical->as_string !~ /^$site_uri/ ) ? 1 : 0;
 }
 
 ### Crawler de site ###
 sub crawler {
-    my ( $start_url, $site_uri ) = @_;
+    my ( $site_uri ) = @_; # Ici était la start URL mais c'est foireux...
 
-    print "crawling [$site_uri] ($start_url) :\n";
-
-    my @to_visit = ( $start_url );
+    my @to_visit = ( $site_uri );
     my @already_visited = ();
     my @outlinks = ();
 
     my $depth = 0;
     my $pages_count = 0;
 
-    while( $depth <= $max_depth && $pages_count <= $max_pages_count && @to_visit ) {
-
-        print "--- before\n";
-        print "... depth = $depth / $max_depth\n";
-        print "... pages_count = $pages_count / $max_pages_count\n";
-        print "... " . scalar( @to_visit ) . " urls to crawl\n";
+    while( $depth < $max_depth && $pages_count < $max_pages_count && @to_visit ) {
 
         my @links = ();
         
         for my $url ( @to_visit ) {
             
-            last unless( $pages_count <= $max_pages_count );
-
-            print "... crawling page [$url]\n";
+            last if( $pages_count >= $max_pages_count );
 
             my $response = $agent->get( $url );
             if( $response->is_success ) {
-#                use YAML::XS;
-#                die( Dump $response );
                 next unless( $response->header( 'content-type' ) =~ m!text/html! );
                 $url = $response->request->uri->canonical->as_string;
+                print "... crawling page [$url]\n";
                 my $result = $scraper->scrape( $response->content );
                 
                 for my $link ( @{ $result->{ links } } ) {
                     my $link_uri = URI->new_abs( $link, $url );
                     if( is_outlink( $site_uri, $link_uri ) ) {
-#                        print "... outlink [$link_uri] found\n";
                         push @outlinks, $link_uri->as_string;
                     }
                     else {
-#                        print "... inlink [$link_uri] found\n";
                         push @links, $link_uri->as_string;
                     }
                  }
@@ -138,8 +163,6 @@ sub crawler {
             push @already_visited, $url;
             $pages_count++;
         }
-
-        print "... found " . scalar( @links ) . "\n";
 
         @to_visit = ();
         
@@ -157,14 +180,29 @@ sub crawler {
 
         $depth++;
 
-        print "--- after\n";
-        print "... depth = $depth / $max_depth\n";
-        print "... pages_count = $pages_count / $max_pages_count\n";
-        print "... " . scalar( @to_visit ) . " urls to crawl\n";
-
     }
 
+    print "... found " . scalar( @outlinks ) . " outlinks\n";
+    
     \@outlinks;
 }
 
+sub export {
+    my $out = io( 'crawl.gdf' );
 
+    "nodedef> name VARCHAR, label VARCHAR, site VARCHAR\n" > $out;
+
+    for my $url ( keys %$nodes ) {
+        my $id = $nodes->{ $url };
+        my $host = URI->new( $url )->host;
+        "v$id, '$url', '$host'\n" >> $out;
+    }
+
+    "edgedef> node1 VARCHAR, node2 VARCHAR, count INT\n" >> $out;
+
+    for my $ids ( keys %$edges ) {
+        my ( $id_source, $id_target ) = split( /-/, $ids );
+        my $count = $edges->{ $ids };
+        "v$id_source, v$id_target, $count\n" >> $out;
+    }
+}
